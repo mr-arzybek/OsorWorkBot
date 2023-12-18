e6119ea2b8f430682e3406e20946b0a64139ea5f
ARDAGER <abdyrakhmanov.arzybek@gmail.com>
2023-12-19 04:27:30 +0600

    upgrate dicker file and cofig file

--- config.py
+++ config.py
@@ -3,26 +3,35 @@ from decouple import config
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 from db.db_psql.db_osor import Database
 
-
+# Инициализация хранилища для хранения состояний FSM
 storage = MemoryStorage()
 
+# Получение значения токена из переменной окружения
 TOKEN = config('TOKEN')
 
-Admins = [908379438, 995712956, 1000541805]
+# Список администраторов
+Admins = [5676759336, 5676759336, 5676759336]
 
-Director = [1738805992, ]
+# Список директоров (если это нужно)
+Director = [5676759336]
+
+# Инициализация бота с использованием токена
 bot = Bot(TOKEN)
 
+# Инициализация диспетчера с использованием бота и хранилища
 dp = Dispatcher(bot=bot, storage=storage)
 
+# Получение значений из переменных окружения
+ip = config('ip')
+PostgresUser = config('PostgresUser')
+PostgresPassword = config('PostgresPassword')
+DATABASE = config('DATABASE')
 
-# ip = config('ip')
-# PostgresUser = config('PostgresUser')
-# PostgresPassword = config('PostgresPassword')
-# DATABASE = config('DATABASE')
+# Формирование строки подключения к PostgreSQL
+POSTGRES_URL = config('POSTGRES_URL', default="postgresql://postgres:123@127.0.0.1:5432/osor_tg_bot")
 
-POSTGRES_URL = "postgresql://postgres:123@127.0.0.1:5432/osor_tg_bot"
+# Получение значения DESTINATION из переменной окружения
 DESTINATION = config('DESTINATION')
+
+# Инициализация объекта для работы с базой данных
 data_b = Database(POSTGRES_URL)
-
-
